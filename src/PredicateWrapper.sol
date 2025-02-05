c48facf39993c73d9a98c6a53d2b2705e9f7195f
Robert Leifke <r.leifke@gmail.com>
2025-02-05 18:00:43 -0500

    fix: remove IHook inheritance

--- src/PredicateWrapper.sol
+++ src/PredicateWrapper.sol
@@ -14,69 +14,9 @@ import {PredicateUniswap} from "./PredicateUniswap.sol";
 import { PredicateClient } from "lib/predicate-std/src/mixins/PredicateClient.sol";
 import { PredicateMessage } from "lib/predicate-std/src/interfaces/IPredicateClient.sol";
 
-contract PredicateWrapper is PredicateClient, IHooks {
+contract PredicateWrapper is PredicateClient {
     constructor(address _serviceManager, string memory _policyID) {
         _initPredicateClient(_serviceManager, _policyID);
-    }
-
-    function beforeInitialize(
-        address sender, 
-        PoolKey calldata key,
-        uint160 sqrtPriceX96 
-    ) external override returns (bytes4) {
-        return (this.beforeInitialize.selector);
-    }
-
-    function afterInitialize(
-        address sender, 
-        PoolKey calldata key, 
-        uint160 sqrtPriceX96,
-        int24 tick
-    ) external override returns (bytes4) {
-        return (this.afterInitialize.selector);
-    }
-
-    function beforeAddLiquidity(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata params,
-        bytes calldata hookData
-    ) 
-        external override returns (bytes4) {
-            return (this.beforeAddLiquidity.selector);
-    }
-
-    function afterAddLiquidity(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata params,
-        BalanceDelta delta,
-        bytes calldata hookData
-    ) external override returns (bytes4, BalanceDelta) {    
-        BalanceDelta hookDelta = BalanceDelta(0,0);
-        return (this.afterAddLiquidity.selector, hookDelta);
-    }
-
-    function beforeRemoveLiquidity(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata params,
-        bytes calldata hookData
-    ) external override returns (bytes4) {
-        return this.beforeRemoveLiquidity.selector;
-    }
-
-    function afterRemoveLiquidity(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata params,
-        BalanceDelta delta,
-        BalanceDelta feesAccrued,
-        BalanceDelta hookDelta,
-        bytes calldata hookData
-    ) external override returns (bytes4, BalanceDelta) {
-        BalanceDelta newHookDelta = BalanceDelta(0,0);
-        return (this.afterRemoveLiquidity.selector, newHookDelta);
     }
 
     function beforeSwap(
@@ -114,36 +54,6 @@ contract PredicateWrapper is PredicateClient, IHooks {
             BeforeSwapDelta swapDelta = BeforeSwapDelta(0,0);
             return (this.beforeSwap.selector, swapDelta, 0);
     }
-
-    function afterSwap(
-            address sender,
-            PoolKey calldata key, 
-            IPoolManager.SwapParams calldata params, 
-            BalanceDelta delta, 
-            bytes calldata hookData
-        ) external override returns (bytes4, int128) {
-            return (this.afterSwap.selector, 0);
-    }
-
-    function beforeDonate(
-            address sender,
-            PoolKey calldata key,
-            uint256 amount0,
-            uint256 amount1,
-            bytes calldata hookData
-        ) external override returns (bytes4) {
-            return this.beforeDonate.selector;
-    }
-
-    function afterDonate(
-            address sender,
-            PoolKey calldata key,
-            uint256 amount0,
-            uint256 amount1,
-            bytes calldata hookData
-        ) external override returns (bytes4) {
-            return this.afterDonate.selector;
-        }
 
     function setPolicy(string memory _policyID) external {
         _setPolicy(_policyID);

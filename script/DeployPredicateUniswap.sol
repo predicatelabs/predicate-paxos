e3261584dbcf2a0b7f2a0f48a0eecebce0dd147b
Robert Leifke <r.leifke@gmail.com>
2025-02-05 10:27:31 -0500

    feat: new deploy script

--- script/DeployPredicateUniswap.sol
+++ script/DeployPredicateUniswap.sol
@@ -1,25 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
-import { PredicateUniswap } from "../src/PredicateUniswap.sol";
-import { IPoolManager } from "v4-core/src/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 
-contract DeployPredicateUniswap is Script {
-    address public poolManagerAddress;
+import {Constants} from "./base/Constants.sol";
+import {PredicateUniswap} from "../src/PredicateUniswap.sol";
+import {HookMiner} from "../test/utils/HookMiner.sol";
 
-    function setUp() public {
-        poolManagerAddress = vm.envAddress("POOL_MANAGER");
-        require(poolManagerAddress != address(0), "Invalid POOL_MANAGER address");
-    }
+/// @notice Mines the address and deploys the Counter.sol Hook contract
+contract PredicateUniswapScript is Script, Constants {
+    function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        // hook contracts must have specific flags encoded in the address
+        uint160 flags = uint160(
+            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+        );
 
-        PredicateUniswap predicateUniswap = new PredicateUniswap(IPoolManager(poolManagerAddress));
-        console.log("PredicateUniswap deployed at:", address(predicateUniswap));
-        console.log("PoolManager address:", poolManagerAddress);
+        // Mine a salt that will produce a hook address with the correct flags
+        bytes memory constructorArgs = abi.encode(POOLMANAGER);
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(CREATE2_DEPLOYER, flags, type(PredicateUniswap).creationCode, constructorArgs);
 
-        vm.stopBroadcast();
+        // Deploy the hook using CREATE2
+        vm.broadcast();
+        PredicateUniswap predicateUniswap = new PredicateUniswap{salt: salt}(IPoolManager(POOLMANAGER));
+        require(address(predicateUniswap) == hookAddress, "PredicateUniswapScript: hook address mismatch");
     }
 }

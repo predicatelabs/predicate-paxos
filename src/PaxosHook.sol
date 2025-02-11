e846037c53eeccddcf170dce0d6b2be8fa312281
Robert Leifke <r.leifke@gmail.com>
2025-02-11 09:50:23 -0500

    featL add paxos hook test

--- src/PaxosHook.sol
+++ src/PaxosHook.sol
@@ -10,7 +10,7 @@ import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
 
-contract PaxosV4Hook is BaseHook {
+contract PaxosHook is BaseHook {
     using PoolIdLibrary for PoolKey;
 
     mapping(PoolId => uint256 count) public beforeSwapCount;

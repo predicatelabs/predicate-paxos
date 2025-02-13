b848c2ab6088d344f2139fedf3d4b20ce2100744
Robert Leifke <r.leifke@gmail.com>
2025-02-13 04:46:44 -0500

    feat: auto wrapping and unwrapping

--- src/PaxosHook.sol
+++ src/PaxosHook.sol
@@ -1,81 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
-
+import {BaseTokenWrapperHook} from "v4-periphery/src/base/hooks/BaseTokenWrapperHook.sol";
+import {wYBSV1} from "lib/ybs-contract/contracts/wYBSV1.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/src/types/Currency.sol";
 
+import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+/// @title Paxos Hook
+/// @notice A hook for auto wrapping and unwrapping YBS, "USDL"
 contract PaxosHook is BaseHook {
     using PoolIdLibrary for PoolKey;
 
-    mapping(PoolId => uint256 count) public beforeSwapCount;
-    mapping(PoolId => uint256 count) public afterSwapCount;
+    wYBSV1 public immutable wYBS;
 
-    mapping(PoolId => uint256 count) public beforeAddLiquidityCount;
-    mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
+    error WithdrawFailed();
 
-    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
-
-    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
-        return Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: true,
-            afterAddLiquidity: false,
-            beforeRemoveLiquidity: true,
-            afterRemoveLiquidity: false,
-            beforeSwap: true,
-            afterSwap: true,
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: false,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
-    }
-
-    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
-        external
-        virtual
-        override
-        returns (bytes4, BeforeSwapDelta, uint24)
+    constructor(IPoolManager _manager, address _wYBS, address _ybs)
+        BaseTokenWrapperHook(
+            _manager,
+            Currency.wrap(_wYBS), 
+            Currency.wrap(_ybs)
+        )
     {
-        beforeSwapCount[key.toId()]++;
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+        wYBS = wYBSV1(payable(_wYBS));
     }
 
-    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
-        external
-        override
-        returns (bytes4, int128)
-    {
-        afterSwapCount[key.toId()]++;
-        return (BaseHook.afterSwap.selector, 0);
+    function deposit(uint256 underlyingAmount) internal override returns (uint256 wrapperAmount) {
+        IERC20Upgradeable(ybsAddress).approve(address(wYBS), underlyingAmount);
+        wYBS.deposit(underlyingAmount, address(this));
+        return underlyingAmount; 
     }
 
-    function beforeAddLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        beforeAddLiquidityCount[key.toId()]++;
-        return BaseHook.beforeAddLiquidity.selector;
-    }
-
-    function beforeRemoveLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        beforeRemoveLiquidityCount[key.toId()]++;
-        return BaseHook.beforeRemoveLiquidity.selector;
+    function withdraw(uint256 wrapperAmount) internal override returns (uint256 underlyingAmount) {
+        wYBS.redeem(wrapperAmount, address(this), address(this));
+        return wrapperAmount; 
     }
 }

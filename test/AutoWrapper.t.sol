a195897518e0c2624bd6d1b1c11b3d8f73d9b59b
Robert Leifke <r.leifke@gmail.com>
2025-02-27 16:21:02 -0500

    feat: consolidate test setup

--- test/AutoWrapper.t.sol
+++ test/AutoWrapper.t.sol
@@ -7,14 +7,46 @@ import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
-import {TestWrapperSetup} from "./helpers/TestWrapperSetup.t.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {wYBSV1} from "../src/paxos/wYBSV1.sol";
+import {HookMiner} from "./utils/HookMiner.sol";
+import {MockERC20} from "./mocks/MockERC20.sol";
 import "forge-std/Test.sol";
 
-contract AutoWrapperTest is TestWrapperSetup, Test {
-    function setUp() public override {
-        super.setUp();
+contract AutoWrapperTest is Test {
+    AutoWrapper public wrapper;
+    MockERC20 public ybs;
+    wYBSV1 public wYBS;
+    MockERC20 public usdc;
+    IPoolManager public poolManager;
+
+    address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
+
+    function setUp() public {
+        poolManager = IPoolManager(0x000000000004444c5dc75cB358380D2e3dE08A90);
+
+        ybs = new MockERC20("YBS Token", "YBS", 18);
+        wYBS = new wYBSV1();
+        usdc = new MockERC20("USD Coin", "USDC", 6);
+
+        PoolKey memory poolKey = PoolKey({
+            currency0: Currency.wrap(address(usdc)),
+            currency1: Currency.wrap(address(wYBS)),
+            fee: 3000,
+            tickSpacing: 60,
+            hooks: IHooks(address(0))
+        });
+
+        uint160 flags = uint160(Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG);
+        bytes memory constructorArgs = abi.encode(poolManager, address(ybs), poolKey);
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(CREATE2_DEPLOYER, flags, type(AutoWrapper).creationCode, constructorArgs);
+
+        wrapper = new AutoWrapper{salt: salt}(poolManager, address(ybs), poolKey);
+        require(address(wrapper) == hookAddress, "AutoWrapper deployed at wrong address");
+
+        ybs.mint(address(this), 1000e18);
+        usdc.mint(address(this), 1000e6);
     }
 
     function testWrapAndSwap() public {

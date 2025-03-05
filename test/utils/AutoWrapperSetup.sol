23f17bf118cb06bb8dc27a6f5ba08d5ad51b5290
Anmol Arora <a53arora@uwaterloo.ca>
2025-03-05 13:00:01 -0800

    more changes

--- test/utils/AutoWrapperSetup.sol
+++ test/utils/AutoWrapperSetup.sol
@@ -1,1 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
+import {PredicateHook} from "../../src/PredicateHook.sol";
+import {SimpleV4Router} from "../../src/SimpleV4Router.sol";
+import {ISimpleV4Router} from "../../src/interfaces/ISimpleV4Router.sol";
+import {YBSV1_1} from "../../src/paxos/YBSV1_1.sol";
+import {wYBSV1} from "../../src/paxos/wYBSV1.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {Constants} from "@uniswap/v4-core/src/../test/utils/Constants.sol";
+import {MetaCoinTestSetup} from "@predicate-test/helpers/utility/MetaCoinTestSetup.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {HookMiner} from "test/utils/HookMiner.sol";
+import {PoolSetup} from "./PoolSetup.sol";
+
+contract AutoWrapperSetup is MetaCoinTestSetup, PoolSetup {
+    PredicateHook public predicateHook;
+    Currency currency0;
+    Currency currency1;
+    int24 tickSpacing = 60;
+    PoolKey poolKey;
+    YBSV1_1 ybs;
+    wYBSV1 wYBS;
+
+    function setUpPredicateHook(
+        address liquidityProvider
+    ) internal {
+        deployPoolManager();
+        deployRouters();
+        deployPosm();
+        (currency0, currency1) = deployAndMintTokens(liquidityProvider, 100_000 ether);
+        vm.startPrank(liquidityProvider);
+        setApprovals(currency0, currency1);
+        vm.stopPrank();
+
+        // create hook here
+        uint160 flags = uint160(Hooks.BEFORE_SWAP_FLAG);
+        bytes memory constructorArgs = abi.encode(manager, swapRouter, address(serviceManager), "testPolicy");
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(address(this), flags, type(PredicateHook).creationCode, constructorArgs);
+
+        hook = new PredicateHook{salt: salt}(manager, swapRouter, address(serviceManager), "testPolicy");
+        require(address(hook) == hookAddress, "Hook deployment failed");
+
+        // initialize the pool
+        poolKey = PoolKey(currency0, currency1, 3000, tickSpacing, IHooks(hook));
+        manager.initialize(poolKey, Constants.SQRT_PRICE_1_1);
+
+        vm.startPrank(liquidityProvider);
+        provisionLiquidity(tickSpacing, poolKey, 100 ether, liquidityProvider, 100_000 ether, 100_000 ether);
+        vm.stopPrank();
+    }
+
+    function deployYBS() internal {
+        YBSV1_1 ybs = new YBSV1_1();
+
+        /// @notice Encode initializer data
+        bytes memory initData = abi.encodeCall(
+            YBSV1_1.initialize,
+            (
+                "Yield Bearing Stablecoin",
+                "YBS",
+                18, // assume 18 decimals for test setup
+                address(0),
+                address(0),
+                address(0),
+                address(0),
+                address(0),
+                address(0)
+            )
+        );
+        /// @notice deploys TransparentUpgradeableProxy
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(ybs), // ybs
+            msg.sender, // proxy
+            initData // initialization call data
+        );
+        ybs = YBSV1_1(address(proxy));
+    }
+
+    function deployWYBS() internal {
+        wYBSV1 wYBS = new wYBSV1();
+
+        /// @notice Encode initializer data
+        bytes memory initData = abi.encodeCall(
+            wYBSV1.initialize,
+            ("Wrapped Yield Bearing Stablecoin", "wYBS", address(ybs), address(0), address(0), address(0))
+        );
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(wYBS), // wYBS
+            msg.sender,
+            initData // initialization call data
+        );
+        wYBS = wYBSV1(address(proxy));
+    }
+
+    function setUpAutoWrapper() internal {
+        // deploy ybs
+        deployYBS();
+        // deploy wYBS
+        deployWYBS();
+    }
+
+    function getPoolKey() public view returns (PoolKey memory) {
+        return poolKey;
+    }
+
+    function getCurrency0() public view returns (Currency) {
+        return currency0;
+    }
+
+    function getCurrency1() public view returns (Currency) {
+        return currency1;
+    }
+
+    function getTickSpacing() public view returns (int24) {
+        return tickSpacing;
+    }
+}
